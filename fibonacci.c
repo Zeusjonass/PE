e88dddb6ad77eb4c3da6e90ee58a7f2ffb6f9976
Zeusjonass <35499170+Zeusjonass@users.noreply.github.com>
2018-01-16 11:51:54 -0600

    Update fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -3,9 +3,10 @@
 int main(int argc, char const *argv[]) {
 
   printf("Introduce la cantidad a imprimir\n");
-  scanf("%s", &n);
+  int n, c;
+  scanf("%d", &n);
 
-  printf("Los primeros %s numeros de la serie Fibonacci son: \n", n);
+  printf("Los primeros %d numeros de la serie Fibonacci son: \n", n);
 
   for (c = 0; c < n; c++) {
     if (c <= 1) {
